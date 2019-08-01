9dc4f56b28d38c44dcf885324997a0647586e8f0
Imanuel Ronaldo <imanuelronaldo@gmail.com>
2019-08-01 23:09:42 +0700

    show to do list

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -7,17 +7,19 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\TodoRepository;
 
 class TodoController extends AbstractController
 {
     /** 
      * @Route("/todo", name="todo")
     */
-
-    public function index(Request $request)
+    public function index(TodoRepository $repository)
     {
-        return $this->render('base.html.twig');
-    }
+        return $this->render('todo/list.html.twig',[
+            'todos'=>$repository->findAll(),
+        ]);
+    } 
 
 
 }
